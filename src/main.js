8e7e42f34cc332628ce226b0c8dfa40c737fd676
John Durrant <johndurrant70@gmail.com>
2018-04-03 09:27:46 +0100

    Initialise web3 in App

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import { store } from './store'
+import {
+  store
+} from './store'
 import firebase from 'firebase'
+import Web3 from 'web3'
 
 Vue.use(Vuetify)
 
@@ -21,17 +24,26 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 const unsubscribe = firebase.auth()
-.onAuthStateChanged((firebaseUser) => {
-  new Vue({
-    el: '#app',
-    router,
-    store,
-    render: h => h(App),
-    created () {
-      if (firebaseUser) {
-        store.dispatch('autoSignIn', firebaseUser)
+  .onAuthStateChanged((firebaseUser) => {
+    new Vue({
+      el: '#app',
+      router,
+      store,
+      render: h => h(App),
+      created () {
+        if (firebaseUser) {
+          store.dispatch('autoSignIn', firebaseUser)
+        }
+        // Create a web3 instance
+        if (typeof web3 !== 'undefined') {
+          console.log('Web3 injected browser: OK.')
+          window.web3 = new Web3(window.web3.currentProvider)
+        } else {
+          console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
+          // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+          window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+        }
       }
-    }
+    })
+    unsubscribe()
   })
-  unsubscribe()
-})
