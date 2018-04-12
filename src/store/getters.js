08288efc236a76aeadb6ffb31dddfb3e4afc90c8
John Durrant <johndurrant70@gmail.com>
2018-04-12 10:04:06 +0100

    getter to filter logged in user

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,10 +1,17 @@
 export default {
-  isAuthenticated (state) {
+  isAuthenticated: state => {
     return state.user.loggedIn !== null &&
     state.user.loggedIn !== undefined &&
     state.user.loggedIn !== false
   },
-  balanceToEther (state) {
+  balanceToEther: state => {
     return state.userDetails.ethBalance === null ? '' : window.web3.utils.fromWei(state.userDetails.ethBalance.toString(), 'ether')
+  },
+  allUsers: (state) => (excludeLoggedinUser = false) => {
+    let allUsers = state.allUsers
+    if (excludeLoggedinUser) {
+      allUsers = allUsers.filter(x => x.ethAccount !== state.userDetails.ethAccount)
+    }
+    return allUsers
   }
 }
