226dfc0b36d4b10b5ff1363b97f4e8ff61a287c4
John Durrant <johndurrant70@gmail.com>
2018-04-22 23:05:29 +0100

    create auction contracts

--- src/components/demoScenarios/Auction.vue
+++ src/components/demoScenarios/Auction.vue
@@ -1,11 +1,29 @@
 <template>
-  <v-container fluid>
-    <v-layout row wrap>
-      <v-flex xs12 class="text-xs-center" mt-5>
-        <h1>Auction</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
+<v-container fluid>
+  <v-layout row wrap>
+    <v-flex xs12 class="text-xs-center" mt-5>
+      <h1>Auction</h1>
+      <h3>Methods Available</h3>
+      <h4>Auction Owner/Controller</h4>
+      <ul>
+        <li>createAction</li>
+        <li>getHighestBid</li>
+        <li>cancelAuction</li>
+        <li>allAuctions</li>
+      </ul>
+      <h4>Auction Bidders</h4>
+      <ul>
+        <li>placeBid</li>
+        <li>withdraw</li>
+      </ul>
+      <h4>Anybody</h4>
+      <ul>
+        <li>allAuctions</li>
+        <li>withdraw</li>
+      </ul>
+    </v-flex>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
