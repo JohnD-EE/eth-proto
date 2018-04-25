379846d721a09e7f6574bbd6dbc71ce8dd3177e0
John Durrant <johndurrant70@gmail.com>
2018-04-25 06:42:54 +0100

    Create Auction on Blockchain

--- src/helpers/demoAuction/auction.js
+++ src/helpers/demoAuction/auction.js
@@ -1,6 +1,15 @@
+import {store} from './../../store'
+
 export default {
   createAuction (payload) {
-    console.log(payload)
-    return true
+    console.log('Creating Auction: ', payload)
+
+    const AuctionFactory = store.state.contracts['AuctionFactory']
+    AuctionFactory.methods.createAuction(payload.bidIncrement, payload.startBlock, payload.endBlock, payload.saleItem)
+    .send({from: payload.hostAddress})
+    .then(receipt => {
+      console.log('Receipt: ', receipt)
+      return receipt
+    })
   }
 }
