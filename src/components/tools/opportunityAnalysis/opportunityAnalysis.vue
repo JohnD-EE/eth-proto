2664fcd3782067a9e027e8b45109275aca5edd39
John Durrant <johndurrant70@gmail.com>
2018-05-08 12:03:52 +0100

    Opportunity Matrix

--- src/components/tools/opportunityAnalysis/opportunityAnalysis.vue
+++ src/components/tools/opportunityAnalysis/opportunityAnalysis.vue
@@ -2,23 +2,32 @@
   <v-layout grid-list-xl>
     <v-flex xs 12 md10 offset-md1 pt-4>
       <v-toolbar color="primary lighten-2" dark>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-toolbar-title>Opporunity Analysis</v-toolbar-title>
+        <v-toolbar-title>Opportunity Analysis</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>search</v-icon>
-        </v-btn>
+        <v-menu bottom>
+            <v-btn slot="activator" icon dark>
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+            <v-list>
+              <v-list-tile @click.native="showBullets = !showBullets">
+                <v-list-tile-title>Toggle Bullets</v-list-tile-title>
+              </v-list-tile>
+              <v-list-tile @click.native="showApplicationTags = !showApplicationTags">
+                <v-list-tile-title>Toggle Tags</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
       </v-toolbar>
 
       <v-card>
         <v-container fluid grid-list-md>
-          <v-layout row wrap>
+          <v-layout row wrap full-height>
             <v-flex xs12 sm6 md6
               v-for="card in cards"
               :key="card.title"
             >
-              <v-card class="mb-4">
-                <div class="grey lighten-2">
+              <v-card class="ma-1" hover>
+                <div class="blue-grey lighten-5">
                 <v-card-title primary-title >
                   <v-flex xs12>
                   <h3 class="headline mb-0">{{card.title}}</h3>
@@ -28,22 +37,21 @@
                 </v-flex>
                 </v-card-title>
               </div>
-
                 <v-divider></v-divider>
-                <v-card-text>
-                  <v-flex xs12>
+                <div style="padding-bottom: 60px">
+                <v-card-text v-show="showBullets">
+                  <v-flex xs12 >
                   <ul class="pl-3">
                     <li
                     v-for="bullet in card.bullets"
-                    :key="application">
+                    :key="bullet">
                     {{bullet}}
                     </li>
                   </ul>
                 </v-flex>
                 </v-card-text>
-
                 <v-divider></v-divider>
-                <v-card-text>
+                <v-card-text v-show="showApplicationTags">
                   <v-chip
                   v-for="application in card.applications"
                   :key="application"
@@ -52,10 +60,12 @@
                   </v-chip>
                 </v-card-text>
                   <v-divider></v-divider>
-                  <v-card-actions>
-                  <v-spacer></v-spacer>
+                </div>
+
+                  <v-card-actions class="bottom-right">
                   <star-rating v-bind:star-size="24"></star-rating>
                 </v-card-actions>
+
               </v-card>
             </v-flex>
           </v-layout>
@@ -65,9 +75,29 @@
   </v-layout>
 </template>
 
+<style>
+.full-height .flex {
+  display: flex;
+}
+.full-height .flex > .card {
+  flex: 1 1 auto;
+}
+.bottom-right {
+  position: absolute;
+  bottom: 8px;
+  right: 16px;
+}
+</style>
+
 <script>
 import StarRating from 'vue-star-rating'
   export default {
+    data () {
+      return {
+        showBullets: false,
+        showApplicationTags: false,
+      }
+    },
     computed: {
       cards () {
         return this.$store.state.opportunityAnalysis
@@ -75,6 +105,9 @@ import StarRating from 'vue-star-rating'
     },
     components: {
       'star-rating': StarRating
+    },
+    methods: {
+      //
     }
   }
 </script>
--- src/store/state.js
+++ src/store/state.js
@@ -39,21 +39,21 @@ export default {
   notificationsQueue: [],
   opportunityAnalysis: [
     {
-      key: 'registration',
-      title: 'Registration',
-      description: 'Formal registration of something, perhaps an asset, or property',
+      key: 'registrar',
+      title: 'Registrar',
+      description: 'Formal registration of something, an asset, or property',
       bullets: [
         'Formal registration, e.g. government',
         'Identification, e.g. an official recognition of a person\'s identity',
         'Proof of ownership, or proof of rights',
         'Licensing, proof of authority, or proof of permission'
       ],
-      applications: ['Voting', 'ID', 'Passports', 'Driver\'s license', 'Membership']
+      applications: ['Voting', 'ID', 'Passports', 'Driver\'s License', 'Membership']
     },
     {
       key: 'governance',
       title: 'Governance',
-      description: 'Distributed, Autonomous Organisations',
+      description: 'Distributed, Autonomous, Organisations',
       bullets: [
         'Administration heavy environments, removing bureaucracy',
         'Accountability of decision making',
@@ -61,22 +61,23 @@ export default {
         'Rules without rulers',
         'Regulatory compliance',
         'Enforced responsibility',
+        'Process automation',
         'Managing many autonomous, accountable, entities',
         'Scrutinisable activities'
       ],
-      applications: ['Govt depts', 'Trade', 'Crowdfunding/ICOs', 'Corporate voting', 'Transparency', 'Accountable org', 'Employee empowerment']
+      applications: ['Govt Depts', 'Trade', 'Crowdfunding/ICOs', 'Corporate Voting', 'Transparency', 'Accountable Orgs', 'Employee Empowerment']
     },
     {
-      key: 'tech-savvy-millenials',
-      title: 'Tech Savvy Millenials',
-      description: 'Markets for the tech savvy. digitally minded, millenials',
+      key: 'tech-savvy-millennials',
+      title: 'Tech-Savvy Millennials',
+      description: 'Markets for tech-savvy, digitally minded, millennials',
       bullets: [
         'Millenials embrace digital tech, they grew up with the Internet',
         'Millenials are more pro-cryptocurrencies',
-        'Young people rejecting big financial institutions, failed by the system',
+        'Young people rejecting big financial institutions, failed by the system, open to alternatives',
         'Lives are lived plugged in and always online'
       ],
-      applications: ['Mobile Apps', 'Alternative Finance', 'Youth Industries', 'Education']
+      applications: ['Mobile Apps', 'Alternative Finance', 'Youthful Industries', 'Education']
     },
     {
       key: 'cumbersome-markets',
@@ -85,9 +86,14 @@ export default {
       bullets: [
         'Broken markets',
         'Slow, clunky, painful, annoying, old markets with many moving parts',
-        'Delays and lots of human intervention required'
+        'Processes prone to delays, or complete market failures',
+        'Lots of human intervention required',
+        'Highly administrative, lots of red tape & paperwork',
+        'High transaction costs',
+        'Significant counter-party risk',
+        'Process automation'
       ],
-      applications: ['Housing market / real estate', 'Supply chain', 'Remittences']
+      applications: ['Property Markets', 'Construction', 'Manufacturing', 'Supply Chain', 'Remittences', 'Government']
     },
     {
       key: 'disintermediation',
@@ -96,10 +102,12 @@ export default {
       bullets: [
         'Over reliance on 3rd party services',
         'Reliance on banks, payments services, lawyers, accountants',
-        'Parisitic 3rd parties',
-        'Reduce wait time, speed up end to end process without 3rd parties'
+        'Removing parisitic 3rd parties',
+        'Peer-to-peer economics',
+        'Reduce wait time, speed up end-to-end process without 3rd parties',
+        'Disrupting service providers, especially the likes of AirBnB and Uber'
       ],
-      applications: ['Retail', 'Transactions', 'Accounting', 'Legal']
+      applications: ['Retail', 'Transactions', 'Accounting', 'Legal', 'Services', 'Trade']
     },
     {
       key: 'sovereignty',
@@ -107,12 +115,12 @@ export default {
       description: 'Giving people sovereign ownership and contole of their data',
       bullets: [
         'Privacy in the hands of the person',
-        'Contol who can access your data',
+        'Control who can access your data',
         'Control how your data is used',
         'Traceability of data usage',
         'Cryptographically secured data'
       ],
-      applications: ['Medical Records', 'Privacy', 'Social Media', 'Data Mining', 'ID']
+      applications: ['Medical Records', 'Privacy', 'Social Media', 'Data Mining', 'ID', 'Data Monetisation']
     },
     {
       key: 'micropayments',
@@ -121,84 +129,130 @@ export default {
       bullets: [
         'Realtime Streaming of payments',
         'Payments too small for Visa/Paypal',
-        '',
-        ''
+        'Small busuinesses',
+        'High velocity transfer of value',
+        'Ad-hoc rewards / incentivisors',
+        'Micro consumption of services'
       ],
-      applications: ['IoT', 'Loyalty Points', 'Tipping', 'Energy Markets']
+      applications: ['IoT', 'Loyalty Points', 'Tipping', 'Energy Markets', 'Trasnport']
     },
     {
       key: 'tokenization',
       title: 'Tokenization',
-      description: 'Ownership and control of any asset via tokens',
+      description: 'Ownership and control of any asset via tokens and cryptography',
       bullets: [
+        'Digital fingerprinting',
+        'Digital representation of an asset, secure transfer of ownership',
         'Utility tokens for applications, fuelling an economic system',
         'Creating new markets with own economic system',
+        'Rights management and monetising intellectual property',
         'Share holdings of an asset (e.g. Gold holdings represented by tokens)',
-        'All money is a token (e.g. Bio Survival Tokens)'
+        'All fiat currencies are tokens (e.g. Bio Survival Tokens)',
+        'Create new markets through monetisation possibilities'
       ],
-      applications: ['Share holdings', 'Utility tokens', 'Attention (advertising)', 'Bandwidth']
+      applications: ['Share holdings', 'Security', 'Utility Tokens', 'Bandwidth', 'Rights Management', 'Monetisation of Anything']
     },
     {
-      key: '',
-      title: '',
-      description: '',
+      key: 'finance',
+      title: 'Finance',
+      description: 'Free market programmable money in financial services',
       bullets: [
-        '',
-        '',
-        '',
-        ''
+        'Pure free market economics',
+        'Cross border transactions',
+        'Uncensored money',
+        'Peer-to-peer finance',
+        'Move large funds with minimal cost or friction',
+        'Banking for the unbanked',
+        'Unbanking the banked'
       ],
-      applications: ['', '', '', '']
+      applications: ['Banking', 'Insurance', 'Economics', 'Payments', 'Funding']
     },
     {
-      key: '',
-      title: '',
-      description: '',
+      key: 'counteparty-risk',
+      title: 'Counterparty Risk',
+      description: 'Markets relying on a high degree of trust between participants',
       bullets: [
-        '',
-        '',
-        '',
-        ''
+        'Fair and predictable trades',
+        'Agreements locked down in code',
+        'Proof that the other party has the means to pay',
+        'Trasnparancy, all parties have full information',
+        'Removal of uncertainty from complex deals',
+        'Trustless markets where consensus is distributed'
       ],
-      applications: ['', '', '', '']
+      applications: ['Gambling', 'Trade', 'Security', 'Fraud Prevention', 'Legal', 'Remittences']
     },
     {
-      key: '',
-      title: '',
-      description: '',
+      key: 'traceabiity',
+      title: 'Traceability',
+      description: 'Using the immutable qualities Blockchain for end-to-end accountability',
       bullets: [
-        '',
-        '',
-        '',
-        ''
+        'End-to-end process accountability',
+        'Sign-off or scanning an item (e.g.RFID) logs immutable record',
+        'Avoiding supply chain scandals',
+        'Traceability from \'farm to fork\'',
+        'Projects - who did what when?',
+        'Tracking value flows inside an organisation, e.g. Government spending (Councils, NHS)',
+        'What happened to my charity donation or government foreign aid contribution?'
       ],
-      applications: ['', '', '', '']
+      applications: ['Supply Chain', 'Compliance', 'Project Management', 'Government Spending', 'Charity', 'Import/Export']
     },
     {
-      key: '',
-      title: '',
-      description: '',
+      key: 'community',
+      title: 'Community',
+      description: 'Community cohesion, identity, and trade through blockchain',
       bullets: [
-        '',
-        '',
-        '',
-        ''
+        'Singly identifyable communities, with common needs',
+        'Creating a branded blockchain based community eco-system',
+        'Easy transactions between members',
+        'Collective funding, collective projects',
+        'Marketing to communities of shared interests',
+        'Incentivising action/behaviour for community benefit'
       ],
-      applications: ['', '', '', '']
+      applications: ['Clubs/Hobbyist', 'Geographical Communities', 'Shared Interests']
     },
     {
-      key: '',
-      title: '',
-      description: '',
+      key: 'distributed-consensus',
+      title: 'Distributed Consensus',
+      description: 'Where we all agree on the cryptographically secured \'Truth\'',
       bullets: [
-        '',
-        '',
-        '',
-        ''
+        'Verifying the truth',
+        'Removing Fraud',
+        'Trust',
+        'Shared Ledgers',
+        'Remove single point of failure',
+        'Syndication of services',
+        'E.g. where aircraft need to agree on which version of software to run',
+        'Immutability',
+        'Proof, reputation and credit worthiness, e.g. Self managed identity proof'
       ],
-      applications: ['', '', '', '']
+      applications: ['Legal', 'Business Identity', 'Safety', 'Fraud', 'Security', 'Trust']
+    },
+    {
+      key: 'uncensorable',
+      title: 'Uncensorable',
+      description: 'You own your money and can decide how to spend it',
+      bullets: [
+        'You money can freely cross borders',
+        'Very large transfers, without banks',
+        'When depositing money in a bank you make an unsecured loan to the bank, the bank owns it and controls how it is used',
+        'Illegal activities',
+        'Privacy'
+      ],
+      applications: ['Uncensored Money', 'Illicit Activities', 'Privacy']
+    },
+    {
+      key: 'political',
+      title: 'Political and Social',
+      description: 'Political and social motivations in favour of decentralisation',
+      bullets: [
+        'Anarchists and libertarians',
+        'Sovereignty of the individual',
+        'Free markets, anarchocapitalism',
+        'Democratising money and markets, levelling the playing field',
+        'Solving social problems, where fiat currency just doesn\'t work',
+        'Money creation, and economics in hands of people, not authoritarian control'
+      ],
+      applications: ['Political Movements', 'Anti-Political Movements', 'Economics', 'Social Problems']
     }
-
-
   ]
 }
