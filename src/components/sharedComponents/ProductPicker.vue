be7ea5a1d23fb9a6da3a23de489a8e2abdf57d5b
John Durrant <johndurrant70@gmail.com>
2018-05-26 14:02:59 +0100

    Product picker, filter by categories

--- src/components/sharedComponents/ProductPicker.vue
+++ src/components/sharedComponents/ProductPicker.vue
@@ -2,33 +2,25 @@
   <v-layout row>
     <v-flex xs12>
       <v-card>
-        <v-toolbar color="primary" dark>
-          <v-toolbar-side-icon></v-toolbar-side-icon>
-          <v-toolbar-title>Products</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn icon>
-            <v-icon>search</v-icon>
-          </v-btn>
-        </v-toolbar>
+        <v-flex xs12>
+          <v-select
+          :items="prodcutCategories"
+          v-model="categoriesFiltered"
+          label="Select"
+          multiple
+          hint="Filter the product list"
+          persistent-hint
+          ></v-select>
+        </v-flex>
         <v-list three-line subheader>
-          <v-subheader>Some Info</v-subheader>
-          <v-list-tile>
-            <v-list-tile-content>
-              <v-list-tile-title>Info</v-list-tile-title>
-              <v-list-tile-sub-title>Words</v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-        <v-divider></v-divider>
-        <v-list three-line subheader>
-          <v-subheader>General</v-subheader>
-          <v-list-tile v-for="(product,p) in products" :key="p">
-
-              <img :src="'../../../static/products/' + product.image" onerror="this.src='../../../static/profile/noImage.jpg'" width="60" class="mr-4">
-
+          <v-list-tile v-for="(product, p) in products" :key="p" v-if="filteredCategory(product.categories)">
+            <img :src="'../../../static/products/' + product.image" onerror="this.src='../../../static/profile/noImage.jpg'" width="76" class="mr-4">
             <v-list-tile-content @click="">
               <v-list-tile-title>{{ product.title }} - {{ product.attributes.size }}</v-list-tile-title>
               <v-list-tile-sub-title>£{{ (product.priceInPence / 100).toFixed(2) }}</v-list-tile-sub-title>
+              <v-list-tile-sub-title>
+                <span class="caption" v-for="(category, c) in product.categories" :key="c">{{ category }}<span v-show="c < (product.categories.length -1)">, </span></span>
+              </v-list-tile-sub-title>
             </v-list-tile-content>
             <v-list-tile-action>
               <v-checkbox v-model="productsSelected[p]"></v-checkbox>
@@ -47,12 +39,26 @@ import ProductsJSON from '../proxyData/products.json'
 export default {
   data () {
     return {
-      productsSelected: [1]
+      productsSelected: [],
+      prodcutCategories: ['food', 'beverages', 'healthy', 'non-alchoholic', 'alcoholic', 'salad', 'vegetables', 'fruit', 'nuts', 'seeds'],
+      categoriesFiltered: ['food', 'beverages', 'healthy', 'non-alchoholic', 'alcoholic', 'salad', 'vegetables', 'fruit', 'nuts', 'seeds']
     }
   },
   computed: {
     products () {
       return ProductsJSON
+    }
+  },
+  methods: {
+    filteredCategory (categories) {
+      let matched = false;
+      let categoriesFiltered = this.categoriesFiltered
+      categoriesFiltered.forEach(res => {
+        if (categories.includes(res)) {
+          matched = true // as soon as we find a match, return true
+        }
+      })
+      return matched
     }
   }
 }
